03137aec786dda16b05d4facff91b3d1631b6038
Evan Crane <evancrane@evancrane-macbookpro2.roam.corp.google.com>
2018-07-17 15:30:48 -0500

    first commit w/ dataproc

---
+++ dataproc/newCluster.sh
@@ -0,0 +1,5 @@
+export PROJECT_ID=$(gcloud config get-value project)
+export BUCKET=$(gcloud config get-value project)
+export REGION=us-central1
+export ZONE=us-central1-c
+export IP=$(curl ifconfig.co)
