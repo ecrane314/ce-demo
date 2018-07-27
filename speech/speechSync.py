aa90aca2cb08370d162f16a749f59da1bfd7c0f5
Evan Crane <evancrane@evancrane-macbookpro2.roam.corp.google.com>
2018-07-27 09:16:51 -0500

    Blob vs bytes, unicode mismatch

--- speech/speechSync.py
+++ speech/speechSync.py
@@ -6,40 +6,39 @@ from sys import argv
 _SAMPLE_RATE=16000
 
 
-def speechRecognize(srcBucket, srcPrefix, destPrefix):
+def speechRecognize(srcBucket):
 	
-	# instantiate clients
-	spclient = speech.SpeechClient()
-        stclient = storage.Client()
+    # instantiate clients
+    spclient = speech.SpeechClient()
+    stclient = storage.Client()
     
-        # speech RecognitionConfig
-	config = speech.types.RecognitionConfig(
-            encoding=LINEAR16 #read in common docs, not python API 
-                # not sure here? speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz=_SAMPLE_RATE,
-            language_code='en-US',
-            # max_alternatives, # Not using but available (0-30)
-            profanity_filter=True,
-            # use_enhanced=True, # Enhanced models  DEPRECATED
-            # model='phone_call', # Choose an enhanced model DEPRECATED
-            # enable_word_time_offsets=TRUE,  # provide time offsets
-            ) 
+    # speech RecognitionConfig
+    config = speech.types.RecognitionConfig(
+    encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz = _SAMPLE_RATE,
+    language_code='en-US',
+    # max_alternatives, # Not using but available (0-30)
+    profanity_filter=True,
+    # use_enhanced=True, # Enhanced models  DEPRECATED
+    # model='phone_call', # Choose an enhanced model DEPRECATED
+    # enable_word_time_offsets=TRUE,  # provide time offsets
+    ) 
 
-        # bucket object iterator
- 	bucket  = stclient.get_bucket(srcBucket)
- 	bucketIter = bucket.list_blobs()
+    # bucket object iterator
+    bucket  = stclient.get_bucket(srcBucket)
+    bucketIter = bucket.list_blobs()
 
     for i in bucketIter:
-    	
+        
         # speech RecognitionAudio
-    	audio = speech.types.RecognitionAudio(i)
-	operation = spclient.recognize(config, audio)
+        audio = speech.types.RecognitionAudio(content=i)
+        operation = spclient.recognize(config, audio)
 
-	# write results to bucket
-    	file = open (i+'script-enh', 'wx')
+        # write results to bucket
+        file = open (i+'transcript', 'xw')
         file.write(operation.transcript)
-    	stclient.writeBlob(target bucket)
-		
+        stclient.writeBlob(srcBucket)
+                
 
 if __name__=="__main__":
-	speechRecognize(argv[1], argv[2], argv[3])
+	speechRecognize(argv[1])
