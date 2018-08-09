792e499c92b2309435889e63a34d6ba911117c42
Evan Crane <ecrane314@gmail.comm>
2018-08-09 08:41:11 -0500

    Adding wav converting because of format issues

---
+++ speech/convert_to_wav.py
@@ -0,0 +1,13 @@
+import os
+
+
+def convert_to_wav():
+    import ffmpy
+
+    for i in filepath:
+
+        function = ffmpy.FFmpeg(
+            inputs=
+            outputs=
+        )
+        function.run()
