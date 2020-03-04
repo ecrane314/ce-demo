e64c412ef28f3ebe1717745bce8ef0a9cdbef362
Evan Crane <ecrane314@gmail.com>
2020-03-04 10:43:45 -0600

    added extra demo startup instructions

--- gke/make-cluster.sh
+++ gke/make-cluster.sh
@@ -1,2 +1,5 @@
 gcloud beta container --project "ce-demo2" clusters create "demo-cluster" --zone "us-central1-a" --no-enable-basic-auth --release-channel "stable" --machine-type "n1-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/cloud-platform","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/ce-demo2/global/networks/custom-vpc" --subnetwork "projects/ce-demo2/regions/us-central1/subnetworks/sn-central1" --default-max-pods-per-node "110" --enable-autoscaling --min-nodes "1" --max-nodes "5" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
 
+
+# once ready,  make the kubeflow pipelines application from marketplace
+# Turn on the notebook machine
