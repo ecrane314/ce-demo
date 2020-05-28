a5d29096060487286a630bf0b4fb73ce689ca183
Evan Crane <ecrane314@gmail.com>
2020-05-28 18:19:03 -0500

    authenticating in app

--- logging/post_log.py
+++ logging/post_log.py
@@ -1,8 +1,39 @@
-import google.cloud.logging
+import json
 import logging
 
-client = google.cloud.logging.Client()
+from google.cloud import logging
+from google.auth import jwt
 
-client.get_default_handler()
-client.setup_logging()
+
+service_account = json.load(open("/home/evancrane/ce-demo2-log.json"))
+audience = "https://www.googleapis.com/auth/logging.write"
+credentials = jwt.Credentials.from_service_account_info(
+    service_account, audience=audience
+)
+
+
+def write_log(logger_name = "test_log"):
+    client = logging.Client("ce-demo2", credentials)
+    client.get_default_handler()
+    client.setup_logging()
+
+
+    # This log can be found in the Cloud Logging console under 'Custom Logs'.
+    logger = client.logger(logger_name)
+
+    # Make a simple text log
+    logger.log_text('Hello, world!')
+
+    # Simple text log with severity.
+    logger.log_text('Goodbye, world!', severity='ERROR')
+
+    # Struct log. The struct can be any JSON-serializable dictionary.
+    logger.log_struct({
+        'name': 'King Arthur',
+        'quest': 'Find the Holy Grail',
+        'favorite_color': 'Blue'
+    })
+
+if __name__ == "__main__":
+    write_log()
 
