705f7bdf425d7c7d19dd08ef046da563ed74fc08
Evan Crane <ecrane314@gmail.com>
2021-03-11 11:51:35 -0600

    Posting local logs

--- logging/post_log.py
+++ logging/post_log.py
@@ -1,33 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 https://cloud.google.com/logging/docs/quickstart-python#linux
 Use this export statement at shell to set your credentials before runtime
 export GOOGLE_APPLICATION_CREDENTIALS=/home/evancrane/ce-demo2-bq-analyst.json
 """
 
-#TODO Logging within Python?  https://realpython.com/python-json/
-#import json
-#import logging
+#Logging within Python?  https://realpython.com/python-json/
+import logging
+import os
 
 #https://googleapis.dev/python/logging/latest/index.html
-from google.cloud import logging
-from google.auth import jwt
+from google.cloud import logging as glogging
+#from google.auth import jwt
 
-null="""
-service_account = json.load(open("/home/evancrane/ce-demo2-log.json"))
-audience = "https://www.googleapis.com/auth/logging.write"
-credentials = jwt.Credentials.from_service_account_info(
-    service_account, audience=audience
-)
-"""
 
-def write_log(logger_name = "test_log"):
-    client = logging.Client()
+def write_log():
+    """Write local test log"""
+    #https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
+    logging.basicConfig(filename='scratch.log', \
+        format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+        #encoding='utf-8'  avail in python 3.9
 
-#TODO Unclear what these do or whether they're needed
-#    handler =  client.get_default_handler()
-#   client.setup_logging()
+    logging.debug("This is a Debug item")
+    logging.info("This is info log")
+    logging.warning('This is a warning log message')
+    logging.error("This is an error")
+    logging.critical("This is critical, STOP")
 
+
+def upload_log(logger_name="test_log"):
+    """Write local log file to Google Cloud Logs """
+    client = glogging.Client()
+
+    #Talks to logger? Confirm what they do
+    #handler =  client.get_default_handler()
+    #client.setup_logging()
 
     # This log can be found in the Cloud Logging console under 'Custom Logs'.
     logger = client.logger(logger_name)
@@ -44,6 +51,13 @@ def write_log(logger_name = "test_log"):
         'quest': 'Find the Holy Grail',
         'favorite_color': 'Blue'
     })
+
+    #service_account = json.load(open("/home/evancrane/ce-demo2-log.json"))
+    #audience = "https://www.googleapis.com/auth/logging.write"
+    #credentials = jwt.Credentials.from_service_account_info(
+    #    service_account, audience=audience
+    #)
+
 
 if __name__ == "__main__":
     write_log()
