0ba250df4987f71f5928d35e218fb7b13a5eb154
Evan Crane <ecrane314@gmail.com>
2021-03-12 17:13:39 -0600

    working

--- logging/post_log.py
+++ logging/post_log.py
@@ -8,59 +8,49 @@ https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
 """
 
 import logging
-import os
 
-from google.cloud import logging as glogging
-#from google.auth import jwt
+import google.cloud.logging
 
 
 def write_log():
     """Write local test log and return logger object"""
     logging.basicConfig(filename='scratch.log', \
         format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
-        #encoding='utf-8'  avail in python 3.9
 
-    logging.debug("This is a Debug item")
-    logging.info("This is info log")
-    logging.warning('This is a warning log message')
-    logging.error("This is an error")
-    logging.critical("This is critical, STOP")
-    return logging.getLogger()
+    logging.debug("This is a DEBUG item")
+    logging.info("This is INFO log")
+    logging.warning('This is a WARNING log message')
+    logging.error("This is an ERROR")
+    logging.critical("This is CRITICAL")
 
-def upload_log(logger_name="test_log"):
+
+#def upload_log(logger_name="test_water_log"):
+def upload_log():
     """Write local log file to Google Cloud Logs """
-    client = glogging.Client()
+    #Cloud logging client
+    client = google.cloud.logging.Client()
+    client.setup_logging(log_level=logging.debug)
 
-    #Talks to logger? Confirm what they do
-    #handler =  client.get_default_handler()
-    #client.setup_logging()
-
+    """
     # This log can be found in the Cloud Logging console under 'Custom Logs'.
     logger = client.logger(logger_name)
 
-    handler = client.setup_logging()
-
     # Make a simple text log
-    logger.log_text('Hello, world! IN UPLOAD')
+    logger.log_text('Hello, world! log_text() IN UPLOAD')
 
     # Simple text log with severity.
-    logger.log_text('Goodbye, world!', severity='ERROR')
+    logger.log_text('Goodbye, world! log_text(severity=ERROR)', severity='ERROR')
 
     # Struct log. The struct can be any JSON-serializable dictionary.
     logger.log_struct({
+        'type': 'log_struct()',
         'name': 'King Arthur',
         'quest': 'Find the Holy Grail',
         'favorite_color': 'Blue'
     })
-
-    #service_account = json.load(open("/home/evancrane/ce-demo2-log.json"))
-    #audience = "https://www.googleapis.com/auth/logging.write"
-    #credentials = jwt.Credentials.from_service_account_info(
-    #    service_account, audience=audience
-    #)
+    """
 
 
 if __name__ == "__main__":
-    local_log = write_log()
-    upload_log(local_log)
-    print(type(local_log))
+    upload_log()
+    write_log()
