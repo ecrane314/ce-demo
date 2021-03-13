33dfca721cf7aa5eb0f44dc08379d291ed4a977e
Evan Crane <ecrane314@gmail.com>
2021-03-12 20:49:49 -0600

    broken, now two handlers, local and cloud,circ ref

--- logging/post_log.py
+++ logging/post_log.py
@@ -1,35 +1,52 @@
 #!/usr/bin/env python3
+
 """
 https://cloud.google.com/logging/docs/quickstart-python#linux
 https://googleapis.dev/python/logging/latest/index.html
-Use an export statement at shell to set your credentials before runtime
-export GOOGLE_APPLICATION_CREDENTIALS=/home/evancrane/ce-demo2-bq-analyst.json
 https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
+https://www.youtube.com/watch?v=jxmzY9soFXg
 """
 
 import logging
 
 import google.cloud.logging
+#from google.cloud.logging.handlers import CloudLoggingHandler
 
 
-def write_log():
-    """Write local test log and return logger object"""
-    logging.basicConfig(filename='scratch.log', \
-        format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
 
-    logging.debug("This is a DEBUG item")
-    logging.info("This is INFO log")
-    logging.warning('This is a WARNING log message')
-    logging.error("This is an ERROR")
-    logging.critical("This is CRITICAL")
+def write_logs():
+    """Setup logging and write test events to local log file"""
+    #create logger and set level
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    #create a format, optional
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    #create handler, set paths, add formatter
+    file_handler = logging.FileHandler('python.log')
+    file_handler.format = formatter
 
 
-#def upload_log(logger_name="test_water_log"):
-def upload_log():
-    """Write local log file to Google Cloud Logs """
+    """Create handler to write to Google Cloud Logs """
     #Cloud logging client
     client = google.cloud.logging.Client()
-    client.setup_logging(log_level=logging.debug)
+    cloud_handler = google.cloud.logging.handlers.CloudLoggingHandler(client)
+    cloud_handler.format = formatter
+
+    #add handlers to logger
+    logger.addHandler(file_handler)
+    logger.addHandler(cloud_handler)
+    #TODO Not sure which this gets: get_default_handler()
+    #logger.addHandler = client.get_default_handler()
+
+
+    logger.debug("This is a DEBUG item")
+    logger.info("This is INFO log")
+    logger.warning('This is a WARNING log message')
+    logger.error("This is an ERROR")
+    logger.critical("This is CRITICAL")
+
 
     """
     # This log can be found in the Cloud Logging console under 'Custom Logs'.
@@ -52,5 +69,4 @@ def upload_log():
 
 
 if __name__ == "__main__":
-    upload_log()
-    write_log()
+    write_logs()
