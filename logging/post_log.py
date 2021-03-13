d7ecec0a824f99d61dca6464a05641015cbddf65
Evan Crane <ecrane314@gmail.com>
2021-03-12 22:00:20 -0600

    working -- local and cloud

--- logging/post_log.py
+++ logging/post_log.py
@@ -13,7 +13,6 @@ import google.cloud.logging
 #from google.cloud.logging.handlers import CloudLoggingHandler
 
 
-
 def write_logs():
     """Setup logging and write test events to local log file"""
     #create logger and set level
@@ -23,20 +22,23 @@ def write_logs():
     #create a format, optional
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-    #create handler, set paths, add formatter
+    #create file handler with path, set formatter
     file_handler = logging.FileHandler('python.log')
-    file_handler.format = formatter
+    file_handler.setFormatter(formatter)
 
 
     """Create handler to write to Google Cloud Logs """
-    #Cloud logging client
+    #create cloud logging client
     client = google.cloud.logging.Client()
+    
+    #Create cloud handler and set formatter
     cloud_handler = google.cloud.logging.handlers.CloudLoggingHandler(client)
-    cloud_handler.format = formatter
+    cloud_handler.setFormatter(formatter)
 
     #add handlers to logger
     logger.addHandler(file_handler)
     logger.addHandler(cloud_handler)
+    print(logger.handlers)
     #TODO Not sure which this gets: get_default_handler()
     #logger.addHandler = client.get_default_handler()
 
@@ -46,26 +48,6 @@ def write_logs():
     logger.warning('This is a WARNING log message')
     logger.error("This is an ERROR")
     logger.critical("This is CRITICAL")
-
-
-    """
-    # This log can be found in the Cloud Logging console under 'Custom Logs'.
-    logger = client.logger(logger_name)
-
-    # Make a simple text log
-    logger.log_text('Hello, world! log_text() IN UPLOAD')
-
-    # Simple text log with severity.
-    logger.log_text('Goodbye, world! log_text(severity=ERROR)', severity='ERROR')
-
-    # Struct log. The struct can be any JSON-serializable dictionary.
-    logger.log_struct({
-        'type': 'log_struct()',
-        'name': 'King Arthur',
-        'quest': 'Find the Holy Grail',
-        'favorite_color': 'Blue'
-    })
-    """
 
 
 if __name__ == "__main__":
