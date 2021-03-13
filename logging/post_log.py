ac854fe8e4ed6babd39e70943138cde275e3645e
Evan Crane <ecrane314@gmail.com>
2021-03-13 08:20:26 -0600

    Try catch for bad auth and explicit json for cron

--- logging/post_log.py
+++ logging/post_log.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 """
-https://cloud.google.com/logging/docs/quickstart-python#linux
-https://googleapis.dev/python/logging/latest/index.html
+How to authenticate manually, explicitly, as in cron where don't know env
+https://cloud.google.com/docs/authentication/production#create_service_account
+
+Loggers and handlers explained
 https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
 https://www.youtube.com/watch?v=jxmzY9soFXg
+
+Logging from python client, logging, export, and handler samples exist
+https://cloud.google.com/logging/docs/quickstart-python#linux
+
+API for Google Logging client library
+https://googleapis.dev/python/logging/latest/index.html
 """
 
 import logging
@@ -14,7 +22,8 @@ import google.cloud.logging
 
 
 def write_logs():
-    """Setup logging and write test events to local log file"""
+    """Setup local logging, infer credentials and setup cloud logging
+     write test events to both local and cloud logs"""
     #create logger and set level
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
@@ -27,10 +36,10 @@ def write_logs():
     file_handler.setFormatter(formatter)
 
 
-    """Create handler to write to Google Cloud Logs """
+    ###  CREATE HANDLER TO WRITE TO GCP CLOUD LOGS  ###
     #create cloud logging client
     client = google.cloud.logging.Client()
-    
+
     #Create cloud handler and set formatter
     cloud_handler = google.cloud.logging.handlers.CloudLoggingHandler(client)
     cloud_handler.setFormatter(formatter)
@@ -38,9 +47,7 @@ def write_logs():
     #add handlers to logger
     logger.addHandler(file_handler)
     logger.addHandler(cloud_handler)
-    print(logger.handlers)
-    #TODO Not sure which this gets: get_default_handler()
-    #logger.addHandler = client.get_default_handler()
+    print("write_logs() handlers: " + str(logger.handlers))
 
 
     logger.debug("This is a DEBUG item")
@@ -50,5 +57,48 @@ def write_logs():
     logger.critical("This is CRITICAL")
 
 
+def write_logs_explicit_credentials():
+    """Setup local logging, explicity define credentials and setup
+     cloud logging, write test events to both local and cloud logs"""
+    #create logger and set level
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    #create a format, optional
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    #create file handler with path, set formatter
+    file_handler = logging.FileHandler('python.log')
+    file_handler.setFormatter(formatter)
+
+    #add handlers to logger
+    logger.addHandler(file_handler)
+
+
+    ###  CREATE HANDLER TO WRITE TO GCP CLOUD LOGS  ###
+    #create cloud logging client
+    try:
+        client = google.cloud.logging.Client.from_service_account_json(\
+            '/home/evancrane/crane-gcp_pi-water-plants.json')
+
+        #Create cloud handler and set formatter
+        cloud_handler = google.cloud.logging.handlers.CloudLoggingHandler(client)
+        cloud_handler.setFormatter(formatter)
+
+        #add cloud handler
+        logger.addHandler(cloud_handler)
+    except FileNotFoundError:
+        logger.error('Cloud logger client not established')
+
+    logger.info("Handlers: %s", logger.handlers)
+
+    logger.debug("This is a DEBUG item")
+    logger.info("This is INFO log")
+    logger.warning('This is a WARNING log message')
+    logger.error("This is an ERROR")
+    logger.critical("This is CRITICAL")
+
+
 if __name__ == "__main__":
-    write_logs()
+    #write_logs()
+    write_logs_explicit_credentials()
