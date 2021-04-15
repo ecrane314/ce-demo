0ec9fa9702f87697ee780b70aa1a39aef5fd2280
Evan Crane <ecrane314@gmail.com>
2021-04-15 17:09:13 -0500

    neighbords

--- scikit/knearest.py
+++ scikit/knearest.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 """
-KNeighborsClassifier, which creates the edges and the classifier based on 
+April 2021
+KNeighborsClassifier, which creates the edges and the classifier based on
     the distances
 NearestNeighbors, which just calculates the distances
 Iris is sample dataset
+Doc https://scikit-learn.org/stable/modules/neighbors.html
+Sample https://scikit-learn.org/stable/auto_examples/neighbors/\
+plot_classification.html#sphx-glr-auto-examples-neighbors-plot-\
+classification-py
 """
 
-print(__doc__)
+#print(__doc__)
 
 import numpy as np
 
@@ -15,9 +20,17 @@ from sklearn import neighbors
 from sklearn import datasets
 
 # Presets
-n_neighbors = 15
+N_NEIGHBORS = 15
+
 iris = datasets.load_iris()
+x = iris.data[:, :2]
+y = iris.target
+print(type(iris))
+print(type(x))
+print(type(y))
 
-kclassifier = KNeighborsClassifier(n_neighbors=3).fit()
-nnclassifier = NearestNeighbors().fit()
+kclassifier = neighbors.KNeighborsClassifier(x=x, y=y, n_neighbors).fit()
+nnclassifier = neighbors.NearestNeighbors(x).fit()
 
+#TODO Fix so it works
+#TODO Fix lint 
