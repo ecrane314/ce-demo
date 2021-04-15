83da3880f1236575a90d3bab9ba963e0d6f6afff
Evan Crane <ecrane314@gmail.com>
2021-04-15 07:59:20 -0500

    nearest neighbors

--- scikit/knearest.py
+++ scikit/knearest.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+KNeighborsClassifier, which creates the edges and the classifier based on 
+    the distances
+NearestNeighbors, which just calculates the distances
+Iris is sample dataset
+"""
+
+print(__doc__)
+
 import numpy as np
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import NearestNeighbors
+from sklearn import neighbors
+from sklearn import datasets
+
+# Presets
+n_neighbors = 15
+iris = datasets.load_iris()
 
 kclassifier = KNeighborsClassifier(n_neighbors=3).fit()
 nnclassifier = NearestNeighbors().fit()
