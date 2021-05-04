10a6b28ac9ceee60d135163cdbe20ad78f34495d
Evan Crane <ecrane314@gmail.com>
2021-05-03 21:45:46 -0500

    scikit learning

--- scikit/knearest.py
+++ scikit/knearest.py
@@ -6,31 +6,69 @@ KNeighborsClassifier, which creates the edges and the classifier based on
     the distances
 NearestNeighbors, which just calculates the distances
 Iris is sample dataset
+
 Doc https://scikit-learn.org/stable/modules/neighbors.html
-Sample https://scikit-learn.org/stable/auto_examples/neighbors/\
-plot_classification.html#sphx-glr-auto-examples-neighbors-plot-\
-classification-py
+Sample https://scikit-learn.org/stable/auto_examples/neighbors/plot_classification.html#sphx-glr-auto-examples-neighbors-plot-classification-py
 """
 
 #print(__doc__)
 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
 
-from sklearn import neighbors
-from sklearn import datasets
+n_neighbors = 15
 
-# Presets
-N_NEIGHBORS = 15
-
+# import some data to play with
 iris = datasets.load_iris()
-x = iris.data[:, :2]
+
+# We only take the first two features. We could avoid this ugly
+# slicing by using a two-dim dataset
+# iris.data is numpy ndarray, first says all rows, second is first 2 cols
+X = iris.data[:, :2]
 y = iris.target
+
+h = .02  # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['orange', 'cyan', 'cornflowerblue'])
+cmap_bold = ['darkorange', 'c', 'darkblue']
+
 print(type(iris))
-print(type(x))
-print(type(y))
+#print(iris.data)
+print(type(X))
+print(X)
 
-kclassifier = neighbors.KNeighborsClassifier(x=x, y=y, n_neighbors).fit()
-nnclassifier = neighbors.NearestNeighbors(x).fit()
+for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
 
-#TODO Fix so it works
-#TODO Fix lint 
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    
+    #TODO what is a Numpy meshgrid? ravel method?
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(figsize=(8, 6))
+    plt.contourf(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=iris.target_names[y],
+                    palette=cmap_bold, alpha=1.0, edgecolor="black")
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (k = %i, weights = '%s')"
+              % (n_neighbors, weights))
+    plt.xlabel(iris.feature_names[0])
+    plt.ylabel(iris.feature_names[1])
+
+#plt.show()
